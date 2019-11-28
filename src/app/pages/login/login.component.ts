9f600209fd283c7860e8361284f68d2c01d34bbb
JulianEcheverri <julianecheverri@outlook.com>
2019-11-28 12:38:31 -0500

    Remember me

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -14,18 +14,24 @@ export class LoginComponent implements OnInit {
 
   constructor(private auth : AuthService, private route: Router) { }
   usuario: UsuarioModel = new UsuarioModel();
+  recordarme = false; // para asociar al check de recordar usuario
   ngOnInit() {
+    // cuando le de recordarme se guardara en el local storage el email asociado
+    // cuando recargue debe hacerse en el ngOnInit
+    if (localStorage.getItem('email')) {
+      this.usuario.email = localStorage.getItem('email');
+      this.recordarme = true;
+    }
   }
 
   login(form: NgForm){
     if (form.invalid) return;
-    // console.log(form);
 
     // muestra un mensaje de alerta con la libreria sweetalert 2
     Swal.fire({
       text: 'Espere por favor...',
       icon: 'info',
-      allowOutsideClick: false
+      allowOutsideClick: false //previene clic afuera
     });
 
     // quita el boton de ok y pone un loading
@@ -34,14 +40,21 @@ export class LoginComponent implements OnInit {
     this.auth.logIn(this.usuario).subscribe(
       respuesta => {
         // el subscribe obtiene la respuesta
-        console.log(respuesta);
+        //console.log(respuesta);
         // para cerrar el alert
         Swal.close();
+
+        // cuando le de recordarme se guardara en el local storage el email asociado
+        // cuando recargue debe hacerse en el ngOnInit
+        if (this.recordarme) {
+          localStorage.setItem('email', this.usuario.email);
+        }
+
         this.route.navigateByUrl('/home');
       },
       // el despues de la funcion de respuesta del subscribe esta el error
       ex =>{
-        console.log(ex.error.error.message);
+        //console.log(ex.error.error.message);
         Swal.fire({
           text: ex.error.error.message,
           title: 'Error al autenticar',
--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,50 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import { FormsModule, NgForm  } from "@angular/forms";
-import { UsuarioModel } from '../../models/usuario.model';
-import { AuthService } from '../../services/auth.service';
-import Swal from 'sweetalert2';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { FormsModule, NgForm } from "@angular/forms";
+import { UsuarioModel } from "../../models/usuario.model";
+import { AuthService } from "../../services/auth.service";
+import Swal from "sweetalert2";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-registro',
-  templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.css']
+  selector: "app-registro",
+  templateUrl: "./registro.component.html",
+  styleUrls: ["./registro.component.css"]
 })
 export class RegistroComponent implements OnInit {
   usuario: UsuarioModel = new UsuarioModel();
-
+  recordarme = false;
   // hacer importacion de FormsModule para uso de ngModel en el formulario
   // e importarlo en app.module.ts
 
-  constructor(private auth : AuthService, private route: Router) { }
+  constructor(private auth: AuthService, private route: Router) {}
 
   ngOnInit() {
     // inicializamos el model usuario
     // this.usuario = new UsuarioModel();
     //this.usuario.email = 'julianecheverri@outlook.com';
-   }
+  }
 
-   onSubmit(form: NgForm){
-     if (form.invalid) return;
-     
+  onSubmit(form: NgForm) {
+    if (form.invalid) return;
+
     // muestra un mensaje de alerta con la libreria sweetalert 2
     Swal.fire({
-      text: 'Espere por favor...',
-      icon: 'info',
+      text: "Espere por favor...",
+      icon: "info",
       allowOutsideClick: false
     });
 
     // quita el boton de ok y pone un loading
     Swal.showLoading();
 
-
     // console.log('formulario enviado');
     // console.log(this.usuario);
     // console.log(form);
-    
+
     // importamos el servicio para tener acceso a la funcion que crea el usuario en firebase
     // firebase da una respuesta y por ello se usa el subscribe
-    
+
     // da uan respuesta similar a esta
     // {
     //   "idToken": "[ID_TOKEN]",
@@ -60,20 +59,27 @@ export class RegistroComponent implements OnInit {
     this.auth.nuevoUsuario(this.usuario).subscribe(
       respuesta => {
         // el subscribe obtiene la respuesta
-        console.log(respuesta);
-         // para cerrar el alert
-         Swal.close();
-         this.route.navigateByUrl('/home');
+        //console.log(respuesta);
+        // para cerrar el alert
+        Swal.close();
+
+        //cuando le de recordarme se guardara en el local storage el email asociado
+        // cuando recargue debe hacerse en el ngOnInit
+        if (this.recordarme) {
+          localStorage.setItem("email", this.usuario.email);
+        }
+
+        this.route.navigateByUrl("/home");
       },
       // el despues de la funcion de respuesta del subscribe esta el error
-      ex =>{
-        console.log(ex.error.error.message);
+      ex => {
+        //console.log(ex.error.error.message);
         Swal.fire({
           text: ex.error.error.message,
-          title: 'Error al autenticar',
-          icon: 'error',
+          title: "Error al autenticar",
+          icon: "error"
         });
       }
     );
-   }
+  }
 }
